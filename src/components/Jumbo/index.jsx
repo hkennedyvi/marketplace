6fdbe2be3c97a45167872c29c43426803ae38377
Howard Kennedy <hkennedyvi@gmail.com>
2020-05-09 21:48:13 -0700

    pages folder created, includes home

---
+++ src/components/Jumbo/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import StaplerIcon from './assets/stapler-80.png';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: "white",
+        height: "500px",
+        width: "100%",
+        borderRadius: "1em",
+        marginTop: "100px",
+        // boxShadow: "5px 5px"
+    },
+}));
+
+function Jumbo() {
+
+    const classes = useStyles();
+
+    return (
+        <Container className={classes.root}>
+            <Typography component="div" />
+        </Container>
+    );
+}
+
+export default Jumbo;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import Jumbo from '../components/Jumbo'
+
+function Home () {
+    return (
+        <div>
+            <Jumbo />
+        </div>
+    )
+}
+
+export default Home;
