9383f63bc30e7ff57dc96b628a91ee694a12206f
Howard Kennedy <hkennedyvi@gmail.com>
2020-06-12 21:13:44 -0700

    card and searchbar style edits

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -36,7 +36,7 @@ export default function ImgMediaCard() {
     const classes = useStyles();
 
     return (
-        <Card className={classes.root}>
+        <Card className={classes.root, "resultCard"}>
             <CardMedia
                 component="img"
                 alt="product"
